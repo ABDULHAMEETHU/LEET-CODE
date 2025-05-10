32b26232a286d3323e9ffc2f7329225cded5a5bf
ABDUL HAMEETHU . M <161139924+ABDULHAMEETHU@users.noreply.github.com>
2025-05-10 20:15:07 +0530

    Time: 2 ms (82.22%), Space: 46.3 MB (8.36%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,10 +1,17 @@
 class Solution {
     public boolean canJump(int[] nums) {
-       int  max_index=0;
-       for(int i=0;i<nums.length;i++){
-        if(i>max_index) return false;
-        max_index=Math.max(max_index,i+nums[i]); //else part
+        int max_index=0;
+        for(int i=0;i<nums.length;i++){
+            if(i+nums[i]>max_index){
+                max_index=i+nums[i];
+            }
+            if(max_index>=nums.length-1){
+                return true;
+            }
+            if(i>=max_index){
+                return false;
+            }
         }
-        return true;
+        return false;
     }
 }
